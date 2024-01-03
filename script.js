492d49769bcd67cd62d3979c4570887ffa14b8cb
wheatbread2056 <134822007+wheatbread2056@users.noreply.github.com>
2024-01-02 20:28:25 -0800

    initial js

---
+++ script.js
@@ -0,0 +1,8 @@
+const songimport = document.getElementById("songimport");
+const playlistimport = document.getElementById("playlistimport");
+songimport.addEventListener("click", function() {
+    alert("test");
+});
+playlistimport.addEventListener("click", function() {
+    alert("test");
+});
