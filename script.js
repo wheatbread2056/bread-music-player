1bb3c54f87395c65d275eb8d5468d09c86599e29
wheatbread2056 <134822007+wheatbread2056@users.noreply.github.com>
2024-01-03 14:46:56 -0800

    fix localstorage

--- script.js
+++ script.js
@@ -11,25 +11,25 @@ function createPlaylist() {
 }
 // colors and themes
 function modifyColor(cvar,id) {
-    color = prompt("Which color would you like to use? (hex, hsl, and rgb supported)")
+    color = prompt("Which color would you like to use? (hex, hsl, and rgb supported)");
     const root = document.documentElement;
-    const indicator = document.getElementById(id)
+    const indicator = document.getElementById(id);
     root.style.setProperty(cvar, color);
-    indicator.textContent = "Selected: "+color
-    localStorage.setItem(cvar,color)
+    indicator.textContent = "Selected: "+color;
+    localStorage.setItem(cvar,color);
 }
 function modifyColor2(cvar,color,id) {
     const root = document.documentElement;
-    const indicator = document.getElementById(id)
+    const indicator = document.getElementById(id);
     root.style.setProperty(cvar, color);
-    indicator.textContent = "Selected: "+color
-    localStorage.setItem(cvar,color)
+    indicator.textContent = "Selected: "+color;
+    localStorage.setItem(cvar,color);
 }
 
 // initialize colors from localstorage
-const col1 = localStorage.getItem("--primary-color");
-const col2 = localStorage.getItem("--secondary-color");
-const col3 = localStorage.getItem("--accent-color");
+var col1 = localStorage.getItem("--primary-color");
+var col2 = localStorage.getItem("--secondary-color");
+var col3 = localStorage.getItem("--accent-color");
 modifyColor2("--primary-color",col1,"col1");
 modifyColor2("--secondary-color",col2,"col2");
 modifyColor2("--accent-color",col3,"col3");
