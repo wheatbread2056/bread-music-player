0dded5d0c9ccf0cffb64e7ca10df218df75c09be
wheatbread2056 <134822007+wheatbread2056@users.noreply.github.com>
2024-01-03 13:54:03 -0800

    save colors to localstorage

--- script.js
+++ script.js
@@ -16,4 +16,20 @@ function modifyColor(cvar,id) {
     const indicator = document.getElementById(id)
     root.style.setProperty(cvar, color);
     indicator.textContent = "Selected: "+color
+    localStorage.setItem(cvar,color)
 }
+function modifyColor2(cvar,color,id) {
+    const root = document.documentElement;
+    const indicator = document.getElementById(id)
+    root.style.setProperty(cvar, color);
+    indicator.textContent = "Selected: "+color
+    localStorage.setItem(cvar,color)
+}
+
+// initialize colors from localstorage
+const col1 = localStorage.getItem("--primary-color");
+const col2 = localStorage.getItem("--secondary-color");
+const col3 = localStorage.getItem("--accent-color");
+modifyColor2("--primary-color",col1,"col1");
+modifyColor2("--secondary-color",col2,"col2");
+modifyColor2("--accent-color",col3,"col3");
