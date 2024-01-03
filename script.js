5c1ba4832c5ab8b72d4b662609a4cd9ee9c28ed2
wheatbread2056 <134822007+wheatbread2056@users.noreply.github.com>
2024-01-03 11:57:08 -0800

    color indicators

--- script.js
+++ script.js
@@ -10,8 +10,10 @@ function createPlaylist() {
     // empty for now
 }
 // colors and themes
-function modifyColor(cvar) {
-    color = prompt("Which color would you like to use? (hex supported)")
+function modifyColor(cvar,id) {
+    color = prompt("Which color would you like to use? (hex, hsl, and rgb supported)")
     const root = document.documentElement;
+    const indicator = document.getElementById(id)
     root.style.setProperty(cvar, color);
+    indicator.textContent = "Selected: "+color
 }
