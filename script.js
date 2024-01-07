9b4bda01f47b95f00ffaa47205ea72bf06e57207
wheatbread2056 <geometrydashiscool222@gmail.com>
2024-01-06 19:13:25 -0800

    Revert "fix color indicators"
    This reverts commit 65898b53b9ed5d3a759dcb4d27d169502fa21dce.

--- script.js
+++ script.js
@@ -30,13 +30,6 @@ function modifyColor2(cvar,color,id) {
 var col1 = localStorage.getItem("--primary-color");
 var col2 = localStorage.getItem("--secondary-color");
 var col3 = localStorage.getItem("--accent-color");
-const ind1 = document.getElementById("col1");
-const ind2 = document.getElementById("col2");
-const ind3 = document.getElementById("col3");
 modifyColor2("--primary-color",col1,"col1");
 modifyColor2("--secondary-color",col2,"col2");
 modifyColor2("--accent-color",col3,"col3");
-// maybe fix indicator color issue in settings
-ind1.textContent = "Selected: "+col1;
-ind2.textContent = "Selected: "+col1;
-ind3.textContent = "Selected: "+col1;
