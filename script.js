17de4ec2819e5bcafb93d2c5158e3cc7be58bd4d
wheatbread2056 <134822007+wheatbread2056@users.noreply.github.com>
2024-01-08 08:27:18 -0800

    default color fix

--- script.js
+++ script.js
@@ -27,15 +27,39 @@ function modifyColor2(cvar,color) {
 var col1 = localStorage.getItem("--primary-color");
 var col2 = localStorage.getItem("--secondary-color");
 var col3 = localStorage.getItem("--accent-color");
-modifyColor2("--primary-color",col1);
-modifyColor2("--secondary-color",col2);
-modifyColor2("--accent-color",col3);
+if (col1 == null) { // first color
+    // do nothing (keep default color)
+} else {
+    modifyColor2("--primary-color",col1);
+}
+if (col2 == null) { // second color
+    // do nothing (keep default color)
+} else {
+    modifyColor2("--secondary-color",col2);
+}
+if (col3 == null) { // third color
+    // do nothing (keep default color)
+} else {
+    modifyColor2("--accent-color",col3);
+}
 // update indicators
 var ind1 = document.getElementById("col1");
 var ind2 = document.getElementById("col2");
 var ind3 = document.getElementById("col3");
 if (ind1 != null && ind2 != null && ind3 != null) {
-    ind1.textContent = "Selected: "+col1;
-    ind2.textContent = "Selected: "+col2;
-    ind3.textContent = "Selected: "+col3;
+    if (col1 == null) { // first color 
+        ind1.textContent = "Selected: Default (purple)";
+    } else {
+        ind1.textContent = "Selected: "+col1;
+    }
+    if (col2 == null) { // second color
+        ind1.textContent = "Selected: Default (purple)";
+    } else {
+        ind1.textContent = "Selected: "+col2;
+    }
+    if (col3 == null) { // third color
+        ind1.textContent = "Selected: Default (purple)";
+    } else {
+        ind1.textContent = "Selected: "+col3;
+    }
 }
